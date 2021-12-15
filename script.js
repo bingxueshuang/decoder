3f07cbfa6471484ac05626a6e36933a60ab469a9
Manojna <manojnakp.201cs230@nitk.edu.in>
2021-12-15 19:40:22 +0530

    Modify layout
    Add links and navigation modal box for layout.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function modalClose(){
 	document.body.classList.remove("modal-open");
-	document.querySelector(".content").removeEventListener('click', modalClose);
+	document.querySelector("div.modal-backdrop").removeEventListener("click", modalClose);
 }
 function modalOpen(){
 	document.body.classList.add("modal-open");
-	document.querySelector(".content").addEventListener('click', modalClose);
+	document.querySelector("div.modal-backdrop").addEventListener("click", modalClose);
 }
