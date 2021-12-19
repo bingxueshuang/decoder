c10d96e4f233bda561191043dda4bda4e2552f58
Manojna <manojnakp.201cs230@nitk.edu.in>
2021-12-19 11:05:50 +0530

    Make changes to modal
    Modal box was not supported on small devices. Also scrolling
    problem when modal was open. Fixed.

--- Folder II/script.js
+++ Folder II/script.js
@@ -1,8 +1,11 @@
+window.scrollVar = '';
 function modalClose(){
 	document.body.classList.remove("modal-open");
 	document.querySelector("div.modal-backdrop").removeEventListener("click", modalClose);
+	window.scrollTo(0, parseInt(window.scrollVar || '0'));
 }
 function modalOpen(){
+	window.scrollVar = window.scrollY;
 	document.body.classList.add("modal-open");
 	document.querySelector("div.modal-backdrop").addEventListener("click", modalClose);
 }
